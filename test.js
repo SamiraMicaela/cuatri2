5dc3c8c35b4ddeaab92ed124f37f6988cf070826
SamiraMicaela <micaelabava@hotmail.es>
2023-08-10 10:25:03 -0300

    second commit

--- test.js
+++ test.js
@@ -8,19 +8,56 @@ class televisor{
     prender(){
         this.prendido= true;
     }
-    subirCanal(){
-        this.canal ++;
-    }
-    bajarCanal(){
-        this.canal = this.canal -1;
-    }
-    cammmbiarCanal(){
-        this.canal 
+    apagar(){
+        this.prendido = false;
     }
 
+    subirCanal(){  
+        if(this.prendido ===true){
+            if (this.canal===99){
+                this.canal = 1;
+            }else{
+                this.canal ++;
+            }
+        }else{
+            mmessage: "Prendelo";
+        }
+    }
+    bajarCanal(){
+        if(this.prendido===true){
+            if(this.canal ===1){
+                this.canal = 99;
+            }else{
+                this.canal = this.canal -1;
+            }
+        }    
+    }
+    cambiarCanal(canalXd){
+        if(this.prendido===true){
+            if(canalXd >1 && canalXd <99){
+                this.canal = canalXd;
+            }
+        }
+    }
+
+    subirVolumen(){
+        if(this.prendido===true){
+            if(this.volumen <100){
+             this.volumen ++;
+            }
+        }
+    }
+    bajarVolumen(){
+        if(this.prendido===true){
+            if(this.volumen>0){
+                this.volumen = this.volumen -1;
+            }
+       } 
+    }
 }
 
 const teleXd = new televisor();
-teleXd.prender();
-teleXd.cambiar(78);
+teleXd.apagar();
+teleXd.cambiarCanal(12);
+teleXd.subirCanal();
 console.log(teleXd);
