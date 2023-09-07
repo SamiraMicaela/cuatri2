994cb6cd440ee729482ddcf5a416037608a81bd7
SamiraMicaela <micaelabava@hotmail.es>
2023-09-07 14:06:35 -0300

    change sc

---
+++ tele.ts
@@ -0,0 +1,84 @@
+class Televisor {
+    private prendido: boolean;
+    private canal: number;
+    private volumen: number;
+
+    constructor() {
+        this.prendido = false;
+        this.canal = 0;
+        this.volumen = 0;
+    }
+
+    prender(): void {
+        this.prendido = true;
+        this.canal = 1;
+    }
+
+    apagar(): void {
+        this.prendido = false;
+    }
+
+    verificarEstado(): void {
+        if (this.prendido) {
+            console.log("El televisor está prendido");
+        } else {
+            console.log("El televisor no funciona si no está prendido");
+        }
+    }
+
+    subirCanal(): void {
+        if (this.prendido) {
+            if (this.canal === 99) {
+                this.canal = 1;
+            } else {
+                this.canal++;
+            }
+        }
+    }
+
+    bajarCanal(): void {
+        if (this.prendido) {
+            if (this.canal === 1) {
+                this.canal = 99;
+            } else {
+                this.canal = this.canal - 1;
+            }
+        }
+    }
+
+    cambiarCanal(canalXd: number): void {
+        if (this.prendido) {
+            if (canalXd > 1 && canalXd < 99) {
+                this.canal = canalXd;
+            }
+        }
+    }
+
+    subirVolumen(): void {
+        if (this.prendido) {
+            if (this.volumen < 100) {
+                this.volumen++;
+            }
+        }
+    }
+
+    bajarVolumen(): void {
+        if (this.prendido) {
+            if (this.volumen > 0) {
+                this.volumen = this.volumen - 1;
+            }
+        }
+    }
+
+    info(): void {
+        if (this.prendido) {
+            const currentDate = new Date();
+            console.log(`Volumen ${this.volumen}, Canal ${this.canal},Hora ${currentDate.toLocaleTimeString()}`);
+        }
+    }
+}
+
+
+const tv1 = new Televisor();
+tv1.info();
+ 
--- test.js
+++ test.js
@@ -1,30 +1,36 @@
 class televisor{
     constructor(){
         this.prendido = false; 
-        this.canal = 1;
+        this.canal = 0;
         this.volumen = 0;
     }
 
     prender(){
         this.prendido= true;
+        this.canal = 1;
     }
     apagar(){
         this.prendido = false;
     }
+    verificarEstado(){
+        if (this.prendido){
+            console.log("El televisor esta prendido");
+        }else{
+            console.log("El televisor no funciona si no esta prendido");
+        }   
+    }
 
     subirCanal(){  
-        if(this.prendido ===true){
+        if(this.prendido){
             if (this.canal===99){
                 this.canal = 1;
             }else{
                 this.canal ++;
             }
-        }else{
-            mmessage: "Prendelo";
         }
     }
     bajarCanal(){
-        if(this.prendido===true){
+        if(this.prendido){
             if(this.canal ===1){
                 this.canal = 99;
             }else{
@@ -33,7 +39,7 @@ class televisor{
         }    
     }
     cambiarCanal(canalXd){
-        if(this.prendido===true){
+        if(this.prendido){
             if(canalXd >1 && canalXd <99){
                 this.canal = canalXd;
             }
@@ -41,23 +47,33 @@ class televisor{
     }
 
     subirVolumen(){
-        if(this.prendido===true){
+        if(this.prendido){
             if(this.volumen <100){
              this.volumen ++;
             }
         }
     }
     bajarVolumen(){
-        if(this.prendido===true){
+        if(this.prendido){
             if(this.volumen>0){
                 this.volumen = this.volumen -1;
             }
-       } 
+        } 
     }
+    info(){
+        if(this.prendido){
+            console.log(`Volumen ${this.volumen},Canal ${this.canal},hora ${currentDate.toLocaleTimeString()} `);
+        }
+    }
+    
 }
 
 const teleXd = new televisor();
-teleXd.apagar();
-teleXd.cambiarCanal(12);
-teleXd.subirCanal();
-console.log(teleXd);
+teleXd.prender();
+teleXd.subirVolumen();
+teleXd.subirVolumen();
+teleXd.subirVolumen();
+teleXd.cambiarCanal(45);
+teleXd.verificarEstado();
+teleXd.info();
+//console.log(teleXd);
